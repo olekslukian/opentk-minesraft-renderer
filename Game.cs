176f020773a356e1f301ab54648fdff8f1068587
olekslukian <ionize155@gmail.com>
2025-03-02 17:06:19 +0100

    Render 2d Textured square

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
-﻿using OpenTK.Graphics.OpenGL4;
+﻿using StbImageSharp;
+using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
@@ -12,14 +13,37 @@ namespace MinesraftRenderer
 
         private readonly float[] _vertices =
         {
-               0f, 0.5f, 0f, //top vertex
-               -0.5f, -0.5f, 0f, //bottom left
-               0.5f , -0.5f, 0f //bottom right
+              -0.5f, 0.5f, 0f, //top left vertex - 0
+              0.5f, 0.5f, 0f, //top right vertex - 1
+              0.5f, -0.5f, 0f, //bottom right vertex - 2
+              -0.5f, -0.5f, 0f //bottom left vertex - 3
         };
+
+        private float[] _textureCoords =
+        {
+            0f, 1f,
+            1f, 1f,
+            1f, 0f,
+            0f, 0f
+        };
+
+        private uint[] _indices =
+        {
+            //top triangle
+            0, 1 ,2,
+            //bottom triangle
+            2, 3, 0
+                 
+        };
+
 
         //Render pipeline vars
         int _vao;
+        int _vbo;
+        int _textureVbo;
         int _shaderProgram;
+        int _ebo;
+        int _textureId;
 
 
         private int _width, _height;
@@ -44,17 +68,40 @@ namespace MinesraftRenderer
             base.OnLoad();
 
             _vao = GL.GenVertexArray();
-            int vbo = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length*sizeof(float), _vertices, BufferUsageHint.StaticDraw);
 
             //Bind VAO
             GL.BindVertexArray(_vao);
+
+            // VERTICES VBO
+
+            _vbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length*sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            //Put the vertex vbo in slot 0 of our vao
+            // Pointer slot 0 of the VAO  to the currently bound VBO
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
             GL.EnableVertexArrayAttrib(_vao, 0);
-
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0); //unbinding vbo
+           
+
+            // TEXTURE VBO
+            _textureVbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _textureVbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, _textureCoords.Length*sizeof(float), _textureCoords, BufferUsageHint.StaticDraw);
+
+            //Pointer slot 1 of the VAO to the currently bount VBO
+            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexArrayAttrib(_vao, 1);
             GL.BindVertexArray(0);
+
+
+
+
+            _ebo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length*sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
 
             //Creating shader program
             _shaderProgram = GL.CreateProgram();
@@ -74,6 +121,26 @@ namespace MinesraftRenderer
             //Deleting shaders
             GL.DeleteShader(vertexShader);
             GL.DeleteShader(fragShader);
+
+            // TEXTURES
+            _textureId = GL.GenTexture();
+            // activate the texture in the unit
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, _textureId);
+
+            //Texture params
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+
+            //Load image
+            StbImage.stbi_set_flip_vertically_on_load(1);
+            ImageResult dirtTexture = ImageResult.FromStream(File.OpenRead("../../../Textures/dirt.png"), ColorComponents.RedGreenBlueAlpha);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, dirtTexture.Width, dirtTexture.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, dirtTexture.Data);
+            //Unbind the texture
+            GL.BindTexture(TextureTarget.Texture2D, 0);
         }
 
         protected override void OnUnload()
@@ -81,6 +148,9 @@ namespace MinesraftRenderer
             base.OnUnload();
 
             GL.DeleteVertexArray(_vao);
+            GL.DeleteBuffer(_vao);
+            GL.DeleteBuffer(_ebo);
+            GL.DeleteTexture(_textureId);
             GL.DeleteProgram(_shaderProgram);
         }
 
@@ -91,8 +161,12 @@ namespace MinesraftRenderer
 
             //Draw triangle
             GL.UseProgram(_shaderProgram);
+
+            GL.BindTexture(TextureTarget.Texture2D, _textureId);
+
             GL.BindVertexArray(_vao);
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
+            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
 
             Context.SwapBuffers();
 
--- Shaders/Default.vert
+++ Shaders/Default.vert
@@ -1,8 +1,11 @@
 #version 330 core
 layout (location = 0) in vec3 aPosition; //vertex coordinate
+layout (location = 1) in vec2 aTexCoord; //Texture coordinate
+
+out vec2 texCoord;
 
 void main() 
 {
 	gl_Position = vec4(aPosition, 1.0); //coordinates
-
+	texCoord = aTexCoord;
 }
